b63e65761f1bbe1c2b73312c73afeebc1390e309
ckbrohog <coltonk97@gmail.com>
2025-06-11 02:05:43 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+// Aerodrome finance data
+const financeData = [
+  { aerodrome: "JFK Airport", revenue: 1200000, expenses: 800000, updated: "2025-06-01" },
+  { aerodrome: "Heathrow", revenue: 1500000, expenses: 1100000, updated: "2025-06-05" },
+  { aerodrome: "Changi", revenue: 900000, expenses: 700000, updated: "2025-06-10" }
+];
